2e009a766410dac06eeb2004a2ded16013e4053c
Yeonho Jeong <yeonho.dev@gamil.com>
2018-08-29 20:36:45 +0900

    Modified Is it New Year? app

--- conditions/application.py
+++ conditions/application.py
@@ -4,8 +4,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.render("/")
+
+@app.route("/")
 def index():
-  now = datetime.datetime.now()
-  new_year = now.month == 1 and now.day ==1
-  return render_template("index.html", new_year=new_year)
+    now = datetime.datetime.now()
+    new_year = now.month == 1 and now.day == 1
+    return render_template("index.html", new_year=new_year)
