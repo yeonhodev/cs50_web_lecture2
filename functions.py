a1d3851a520daecd6025ad48944060fefedcb564
Yeonho Jeong <yeonho.dev@gamil.com>
2018-08-29 11:31:14 +0900

    Added main function

--- functions.py
+++ functions.py
@@ -2,7 +2,11 @@ def square(x):
     return x * x
 
 
-for i in range(10):
-    print(f"{i} squared is {square(i)}")
-    # Older way to write formatted string on Python version 2.7 is
-    # print("{} squared is {}".format(i, square(i)))
+def main():
+  for i in range(10):
+      print(f"{i} squared is {square(i)}")
+      # Older way to write formatted string on Python version 2.7 is
+      # print("{} squared is {}".format(i, square(i)))
+
+if __name__ == "__main__":
+  main()
