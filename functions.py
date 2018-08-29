d8feadb633600710c87a2aaf8b0b70e79ce77552
Yeonho Jeong <yeonho.dev@gamil.com>
2018-08-29 10:53:18 +0900

    Modified functions.py

--- functions.py
+++ functions.py
@@ -4,3 +4,5 @@ def square(x):
 
 for i in range(10):
     print(f"{i} squared is {square(i)}")
+    # Older way to write formatted string on Python version 2.7 is
+    # print("{} squared is {}".format(i, square(i)))
